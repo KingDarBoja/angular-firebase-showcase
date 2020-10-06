942a2f8867d3460b2a99d03463104269fcaa50bd
KingDarBoja <stevenbojato04@gmail.com>
2020-10-06 16:55:59 -0500

    feat: check if offline on app component

--- apps/pwa-table-pagination/src/app/app.component.ts
+++ apps/pwa-table-pagination/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
+
+import { ConnectionService } from './services/connection.service';
 
 @Component({
   selector: 'afs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pwa-table-pagination';
+  online = true;
+
+  private connectionSub: Subscription;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private connection: ConnectionService,
+  ) {}
+
+  ngOnInit(): void {
+    this.connectionSub = this.connection.connectionStateObservable$.subscribe(online => {
+      this.online = online;
+      if (!this.online) {
+        this.snackBar.open('You are offline!', 'OK', { duration: 3000 });
+      } else {
+        this.snackBar.dismiss();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.connectionSub.unsubscribe();
+  }
 }
--- apps/pwa-table-pagination/src/app/services/connection.service.ts
+++ apps/pwa-table-pagination/src/app/services/connection.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Observable, fromEvent, merge, of, BehaviorSubject, } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Observable, fromEvent, merge, of } from 'rxjs';
 import { mapTo } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConnectionService {
   connectionStateObservable$: Observable<boolean>;
 
-  constructor() { }
-
-  start() {
-    this.initSubjects();
+  constructor(@Inject(PLATFORM_ID) platformId: object) {
+    if (isPlatformBrowser(platformId)) {
+      this.initSubjects();
+    }
   }
 
   initSubjects() {
