12652aafa65ba03730d284a20f5050f048097f7f
KingDarBoja <stevenbojato04@gmail.com>
2020-10-06 14:23:04 -0500

    initial commit

---
+++ apps/pwa-table-pagination/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-firebase-showcase-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'pwa-table-pagination';
+}
